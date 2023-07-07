5668e6c31feea19c3a27d9b872266c9a85db0cd5
samat <somych1@gmail.com>
2023-07-07 14:39:14 -0500

    postMessage fixed

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -41,8 +41,12 @@ public class SocialMediaController {
         Message postedMessage = messageServiceLayer.createMessage(newMessage);
         System.out.println("this is postMessage 1111111111111111111");
         if(postedMessage == null){
+
+            System.out.println("this is postMessage 2222222222222222222");
             context.status(400);
         } else {
+
+            System.out.println("this is postMessage 3333333333333333");
             context.status(200);
             context.json(mapper.writeValueAsString(postedMessage));
         }
